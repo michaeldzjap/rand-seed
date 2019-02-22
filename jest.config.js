d8913766654d48ef7b87711eaf53aa5a106453f3
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2019-02-22 16:13:14 +0100

    Update Jest config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+    ],
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+    ],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json',
+        },
+    },
+    collectCoverage: true,
+};
