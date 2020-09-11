07908bc2e3c3e00b49704e04bd3a0600db57f4fc
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2020-09-11 17:13:11 +0200

    wp

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: [
-        '**/__tests__/**/*.[jt]s?(x)',
-    ],
-    testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-    ],
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
     globals: {
         'ts-jest': {
             tsConfig: 'tsconfig.test.json',
