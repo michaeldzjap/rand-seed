4ceec41f8c0564923959dbd8e32daee58d190051
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2022-10-15 11:04:18 +0200

    Update dependencies + rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
-// import eslint from '@rollup/plugin-eslint';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 const local = process.env.NODE_ENV === 'local';
 
@@ -19,9 +18,7 @@ export default {
         include: 'src/**',
     },
     plugins: [
-        // eslint(),
         typescript({
-            typescript: require('typescript'),
             sourceMap: local,
         }),
         commonjs(),
