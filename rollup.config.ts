27e8d261181f13bce97d58e933046c9ace9203fe
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2024-08-18 15:43:28 +0200

    Revert latest changes

--- rollup.config.ts
+++ rollup.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
         typescript({ sourceMap: local }),
         terser(),
         (() => ({
-            name: 'patchCJjs',
+            name: 'collectCjsTypeDeclaration',
             writeBundle: {
                 sequential: true,
                 order: 'post',
@@ -41,15 +41,31 @@ export default defineConfig({
                         return;
                     }
 
-                    fs.writeFile('./dist/cjs/index.d.ts', 'export * from \'../es/index\';\nexport { default } from \'../es/index\';', (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
+                    fs.readdir('./dist/es', { recursive: true }, (err, list) => {
+                        for (const item of list) {
+                            if (item.toString().endsWith('.js')) {
+                                continue;
+                            }
 
-                    fs.writeFile('./dist/cjs/package.json', '{\n\t"type": "commonjs"\n}', (err) => {
-                        if (err) {
-                            console.error(err);
+                            if (fs.lstatSync(`./dist/es/${item.toString()}`).isDirectory()) {
+                                if (!fs.existsSync(`./dist/cjs/${item.toString()}`)) {
+                                    fs.mkdirSync(`./dist/cjs/${item.toString()}`, { recursive: true });
+                                }
+
+                                continue;
+                            }
+
+                            fs.copyFile(`./dist/es/${item}`, `./dist/cjs/${item}`, (err) => {
+                                if (err) {
+                                    console.error(err);
+                                }
+
+                                fs.writeFile('./dist/cjs/package.json', '{\n\t"type": "commonjs"\n}', (err) => {
+                                    if (err) {
+                                        console.error(err);
+                                    }
+                                });
+                            });
                         }
                     });
                 },
